159bc7faaa5417ce79c9fb9b5619667ddb9a8543
Mateo Piñeiro <mateopineiro2001@gmail.com>
2023-08-16 08:54:32 -0300

    replace signInWithRedirect with signInWithPopup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import { getRedirectResult, signInWithRedirect } from "firebase/auth";
+import {
+  getRedirectResult,
+  signInWithPopup,
+  signInWithRedirect,
+} from "firebase/auth";
 import { auth, provider } from "../../lib/firebase-config";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
@@ -9,37 +13,20 @@ export default function SignIn() {
   const [loadingState, setLoadingState] = useState("");
   const router = useRouter();
 
-  useEffect(() => {
-    const authenticate = async () => {
-      try {
-        const userCred = await getRedirectResult(auth);
-
-        if (!userCred) return;
-        setLoadingState("Getting redirect data...");
-
-        const response = await fetch("/api/login", {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${await userCred.user.getIdToken()}`,
-          },
-        });
-
-        if (response.status === 200) {
-          setLoadingState("Done!");
-          router.push("/protected");
-        }
-      } catch (err) {
-        setLoadingState("getRedirectResult went wrong!");
-        console.log("getRedirectResult error", err);
-      }
-    };
-    authenticate();
-  }, [router]);
-
   async function signIn() {
     setLoadingState("redirecting...");
     try {
-      await signInWithRedirect(auth, provider);
+      const userCred = await signInWithPopup(auth, provider);
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${await userCred.user.getIdToken()}`,
+        },
+      });
+      if (response.status === 200) {
+        setLoadingState("Done!");
+        router.push("/protected");
+      }
     } catch (err) {
       console.log("signInWithRedirect error", err);
     } finally {
